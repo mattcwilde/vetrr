f8b61ec0aff891b9aa2e0786bc8a056d64c7cc0c
Matt Wilde <mwilde@uw.edu>
2018-10-14 16:22:19 -0700

    test_xspecgui works

--- vetrr/tests/test_vetrr_gui.py
+++ vetrr/tests/test_vetrr_gui.py
@@ -4,16 +4,20 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 
 import pytest
 import os
+import sys
 from pkg_resources import resource_filename
 
+from PyQt5.QtWidgets import QApplication
 from linetools.guis import xspecgui
 
+app = QApplication(sys.argv)
 # Set of Input lines
 def data_path(filename):
-    data_dir = os.path.join(os.path.dirname(__file__), '../files')
+    data_dir = os.path.join(os.path.dirname(__file__), 'files')
     return os.path.join(data_dir, filename)
 
 def test_xspecgui():
     # Init
     spec_fil = data_path('J105935.22+144406.3.fits.gz')
+    print(spec_fil)
     xsgui = xspecgui.XSpecGui(spec_fil, unit_test=True)
