64a6b71bedc7b465fd9a9c8aa0b166c959527bb4
Matt Wilde <mwilde@uw.edu>
2018-10-14 16:13:21 -0700

    updateing test file

--- vetrr/tests/test_vetrr_gui.py
+++ vetrr/tests/test_vetrr_gui.py
@@ -3,11 +3,17 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 # TEST_UNICODE_LITERALS
 
 import pytest
-import numpy as np
 import os
 from pkg_resources import resource_filename
 
+from linetools.guis import xspecgui
 
+# Set of Input lines
 def data_path(filename):
-    data_dir = resource_filename('vetrr', 'vetrr/tests/')
+    data_dir = os.path.join(os.path.dirname(__file__), '../files')
     return os.path.join(data_dir, filename)
+
+def test_xspecgui():
+    # Init
+    spec_fil = data_path('J105935.22+144406.3.fits.gz')
+    xsgui = xspecgui.XSpecGui(spec_fil, unit_test=True)
