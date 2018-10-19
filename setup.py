e095b6ab1cb8f15bde951e970cf1e5c9f4901775
Matt Wilde <mwilde@uw.edu>
2018-10-19 11:37:48 -0700

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_scripts():
 scripts = get_scripts()
 
 setup(name='vetrr',
-      version='0.1.5',
+      version='0.1.6',
       description='vet redrock outputs',
       url='http://github.com/mattcwilde/vetrr',
       author='Matthew Wilde',
